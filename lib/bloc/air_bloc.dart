c4041f3a30398b14e386489381ae01f9f2290f6f
Jinsujin <sylpid003@gmail.com>
2020-09-23 00:44:11 +0900

    Bloc 패턴 적용, UI에서 뷰데이터, 비지니스 로직 분리

--- lib/bloc/air_bloc.dart
+++ lib/bloc/air_bloc.dart
@@ -1,1 +1,30 @@
-class AirBloc {}
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
+import 'package:flutter_dust/models/air_result.dart';
+
+class AirBloc {
+  final _airSubject = BehaviorSubject<AirResult>();
+
+  /// 앱이 시작하자 마자, API 로 데이터를 기져옴
+  AirBloc() {
+    fetch();
+  }
+
+// 비동기 데이터는 Future 로 받아야 한다
+  Future<AirResult> fetchData() async {
+    var response = await http.get(
+        'http://api.airvisual.com/v2/nearest_city?key=2b265a6b-83d7-4e09-88e3-8854aefa9a78');
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    return result;
+  }
+
+  void fetch() async {
+    print('start fetch');
+    var airResult = await fetchData();
+    _airSubject.add(airResult);
+  }
+
+// _airSubject의 스트림을 getter로 외부에 꺼낼수 있도록 함
+  Stream<AirResult> get airResult => _airSubject.stream;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dust/models/AirResult.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter_dust/bloc/air_bloc.dart';
+import 'package:flutter_dust/models/air_result.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+final airBloc = AirBloc();
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,105 +30,93 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
-  AirResult _result;
-
-// 비동기 데이터는 Future 로 받아야 한다
-  Future<AirResult> fetchData() async {
-    var response = await http.get(
-        'http://api.airvisual.com/v2/nearest_city?key=2b265a6b-83d7-4e09-88e3-8854aefa9a78');
-    AirResult result = AirResult.fromJson(json.decode(response.body));
-    return result;
-  }
-
-// 초기에 수행되는 생명주기 함수
-  @override
-  void initState() {
-    super.initState();
-
-// 비동기로 데이터를 가져왔을경우, 해당 데이터를 result 에 넣고 setState 로 화면을 갱신한다
-    fetchData().then((airResult) {
-      setState(() {
-        _result = airResult;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: _result == null
-            ? Center(child: CircularProgressIndicator())
-            : Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+        body: StreamBuilder<AirResult>(
+            stream: airBloc.airResult,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return buildBody(snapshot.data);
+              } else {
+                return CircularProgressIndicator();
+              }
+            }));
+  }
+
+  Widget buildBody(AirResult result) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('현재 위치 미세먼지', style: TextStyle(fontSize: 30)),
+            SizedBox(
+              height: 16,
+            ),
+            Card(
+                child: Column(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
-                      Text('현재 위치 미세먼지', style: TextStyle(fontSize: 30)),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Card(
-                          child: Column(
-                        children: <Widget>[
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Text('얼굴사진'),
-                                Text('${_result.data.current.pollution.aqius}',
-                                    style: TextStyle(fontSize: 40)),
-                                Text(getString(_result),
-                                    style: TextStyle(fontSize: 20)),
-                              ],
-                            ),
-                            color: getColor(_result),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Image.network(
-                                        'https://airvisual.com/images/${_result.data.current.weather.ic}.png',
-                                        width: 32,
-                                        height: 32),
-                                    SizedBox(width: 16),
-                                    Text('${_result.data.current.weather.tp}',
-                                        style: TextStyle(fontSize: 16)),
-                                  ],
-                                ),
-                                Text('습도 ${_result.data.current.weather.hu}%',
-                                    style: TextStyle(fontSize: 16)),
-                                Text('풍속 ${_result.data.current.weather.ws}m/s',
-                                    style: TextStyle(fontSize: 20)),
-                              ],
-                            ),
-                          )
-                        ],
-                      )),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: RaisedButton(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 15.0, horizontal: 50),
-                            onPressed: () {},
-                            color: Colors.orange,
-                            child: Icon(
-                              Icons.refresh,
-                              color: Colors.white,
-                            )),
-                      )
+                      Text('-'),
+                      Text('${result.data.current.pollution.aqius}',
+                          style: TextStyle(fontSize: 40)),
+                      Text(getString(result), style: TextStyle(fontSize: 20)),
                     ],
                   ),
+                  color: getColor(result),
                 ),
-              ));
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.network(
+                              'https://airvisual.com/images/${result.data.current.weather.ic}.png',
+                              width: 32,
+                              height: 32),
+                          SizedBox(width: 16),
+                          Text('${result.data.current.weather.tp}',
+                              style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                      Text('습도 ${result.data.current.weather.hu}%',
+                          style: TextStyle(fontSize: 16)),
+                      Text('풍속 ${result.data.current.weather.ws}m/s',
+                          style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                )
+              ],
+            )),
+            SizedBox(
+              height: 16,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: RaisedButton(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 50),
+                  onPressed: () {
+                    airBloc.fetch();
+                  },
+                  color: Colors.orange,
+                  child: Icon(
+                    Icons.refresh,
+                    color: Colors.white,
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   Color getColor(AirResult result) {
